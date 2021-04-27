1b7fdf734ef7afd86fceda49c996d3b574983f39
flocker1 <40889133+flocker1@users.noreply.github.com>
2021-04-27 23:58:56 +0800

    backup

--- core/settings.py
+++ core/settings.py
@@ -137,3 +137,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+X_FRAME_OPTIONS = "SAME_ORIGIN"
--- quiz/static/quiz/quiz.js
+++ quiz/static/quiz/quiz.js
@@ -8,7 +8,32 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#show-all-answers').addEventListener('click', function() {
         handleShowAllAnswers()
     })
+    document.querySelectorAll('.ignore-question').forEach(element => {
+        element.addEventListener('click', function() {
+            handleIgnoreQuestion()
+        })
+    })
 })
+
+function handleIgnoreQuestion() {
+    days = event.target.dataset.ignoreFor
+    dataId = event.target.dataset.id.split('-')
+    questionId = dataId[dataId.length - 1]
+    fetch(`/api/v1/ignore/${questionId}?days=${days}`, {
+        method: 'PATCH',
+        credentials: 'same-origin',
+        headers: {
+            'X-CSRFToken': getCookie('csrftoken')
+        }
+    })
+    .then(function(response) {
+        console.log(response.status)
+        return response.json()
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+}
 
 function handleShowAnswer() {
     answer = document.getElementById(`answer-${event.target.id}`)
@@ -35,4 +60,20 @@ function handleShowAllAnswers() {
             element.style.display = 'none'
         }
     });
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
