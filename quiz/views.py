4ca72ff0b01ddb4df5022bc4b5c541d0009b67dc
flocker1 <40889133+flocker1@users.noreply.github.com>
2021-05-22 14:41:06 +0800

    checkbox filter

--- quiz/views.py
+++ quiz/views.py
@@ -8,7 +8,7 @@ from .forms import QuestionFilterForm, QuestionFilterForm1
 def index(request):
     if request.GET.get('number_of_questions'):
         # Return filter form to user
-        filter_form = QuestionFilterForm(request.GET)
+        form = QuestionFilterForm1(request.GET)
         # Check if user is wanting specific categories
         number_of_questions_requested = int(request.GET.get('number_of_questions'))
         if request.GET.get('categories'):
@@ -23,11 +23,10 @@ def index(request):
             questions = Question.objects.exclude(ignore_until__gte=timezone.now()).filter(hide_indefinitely=False).order_by('?')[:number_of_questions_requested]
 
     else:
-        filter_form = QuestionFilterForm()
+        form = QuestionFilterForm1()
         questions = Question.objects.exclude(ignore_until__gte=timezone.now()).filter(hide_indefinitely=False).order_by('?')[:10].select_related('category')
     context = {
         "questions":questions,
-        "filter_form":filter_form,
-        "form": QuestionFilterForm1(),
+        "form": form,
     }
     return render(request, 'quiz/index.html', context)
